5de6faee942787c606830febe3a1a1031492ad34
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-02-18 16:36:49 +0100

    tried to fix soot compat via mixins

---
+++ src/main/java/teamroots/embers/mixin/LateMixin.java
@@ -0,0 +1,19 @@
+package teamroots.embers.mixin;
+
+import org.spongepowered.include.com.google.common.collect.ImmutableList;
+import teamroots.embers.Embers;
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LateMixin implements ILateMixinLoader {
+    public static final List<String> modMixins = ImmutableList.of(
+            //"soot"
+    );
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return modMixins.stream().map(mod -> "mixin."+ Embers.MODID +"." + mod + ".json").collect(Collectors.toList());
+    }
+}
