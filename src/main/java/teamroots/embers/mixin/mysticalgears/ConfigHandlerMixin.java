fa1db78f9a070c4913155a4dac7eb809652f9d7f
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-02-19 21:10:14 +0100

    got my mixins to load

--- src/main/java/teamroots/embers/mixin/mysticalgears/ConfigHandlerMixin.java
+++ src/main/java/teamroots/embers/mixin/mysticalgears/ConfigHandlerMixin.java
@@ -9,28 +9,28 @@ import teamroots.embers.config.ConfigMaterial;
 @Mixin(value = ConfigHandler.class, remap = false)
 public class ConfigHandlerMixin {
 
-    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lcom/rcx/mystgears/ConfigHandler;aluminium:Ljava/lang/Boolean;"))
-    private static Boolean aluminium() {
-        return ConfigMaterial.ALUMINUM.isNotOff() && ConfigHandler.aluminium;
+    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;enableAluminum:Z"))
+    private static boolean aluminium() {
+        return ConfigMaterial.ALUMINUM.isNotOff();
     }
 
-    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lcom/rcx/mystgears/ConfigHandler;bronze:Ljava/lang/Boolean;"))
-    private static Boolean bronze() {
-        return ConfigMaterial.BRONZE.isNotOff() && ConfigHandler.bronze;
+    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;enableBronze:Z"))
+    private static boolean bronze() {
+        return ConfigMaterial.BRONZE.isNotOff();
     }
 
-    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lcom/rcx/mystgears/ConfigHandler;electrum:Ljava/lang/Boolean;"))
-    private static Boolean electrum() {
+    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;enableElectrum:Z"))
+    private static boolean electrum() {
         return ConfigMaterial.ELECTRUM.isNotOff() && ConfigHandler.electrum;
     }
 
-    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lcom/rcx/mystgears/ConfigHandler;nickel:Ljava/lang/Boolean;"))
-    private static Boolean nickel() {
+    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;enableNickel:Z"))
+    private static boolean nickel() {
         return ConfigMaterial.NICKEL.isNotOff() && ConfigHandler.nickel;
     }
 
-    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lcom/rcx/mystgears/ConfigHandler;tin:Ljava/lang/Boolean;"))
-    private static Boolean tin() {
+    @Redirect(method = "syncConfig", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;enableTin:Z"))
+    private static boolean tin() {
         return ConfigMaterial.TIN.isNotOff() && ConfigHandler.tin;
     }
 }
---
+++ src/main/java/teamroots/embers/mixin/mysticalgears/ItemGearMixin.java
@@ -0,0 +1,48 @@
+package teamroots.embers.mixin.mysticalgears;
+
+import com.rcx.mystgears.item.ItemGear;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import teamroots.embers.config.ConfigMachine;
+import teamroots.embers.config.ConfigMisc;
+import teamroots.embers.recipe.ItemStampingRecipe;
+import teamroots.embers.register.ItemRegister;
+
+@Mixin(value = ItemGear.class, remap = false)
+public class ItemGearMixin {
+
+    @Shadow public String name;
+
+    @Redirect(method = "registerRecipe", at = @At(value = "FIELD", target = "Lteamroots/embers/ConfigManager;stampGearAmount:I"))
+    private int stampGearAmount() {
+        return ConfigMachine.STAMPER.stampGearAmount;
+    }
+
+    @Redirect(method = "registerRecipe", at = @At(value = "FIELD", target = "Lteamroots/embers/RegistryManager;stamp_gear:Lnet/minecraft/item/Item;"))
+    private Item stampGear() {
+        return ItemRegister.STAMP_GEAR;
+    }
+
+//    @ModifyArg(method = "registerRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/fluids/FluidRegistry;getFluidStack(Ljava/lang/String;I)Lnet/minecraftforge/fluids/FluidStack;"), index = 1)
+//    private int stampGearAmount(int amount) {
+//        return ConfigMachine.STAMPER.stampGearAmount * ConfigMisc.ingotFluidAmount;
+//    }
+//
+//    @ModifyArg(method = "registerRecipe", at = @At(value = "INVOKE", target = "Lteamroots/embers/recipe/ItemStampingRecipe;<init>(Lnet/minecraft/item/crafting/Ingredient;Lnet/minecraftforge/fluids/FluidStack;Lnet/minecraft/item/crafting/Ingredient;Lnet/minecraft/item/ItemStack;)V"), index = 2)
+//    private Ingredient stampGearOutput(Ingredient input) {
+//        return Ingredient.fromItem(ItemRegister.STAMP_GEAR);
+//    }
+//
+//    @Redirect(method = "registerRecipe", at = @At(value = "INVOKE", target = "Lteamroots/embers/recipe/ItemStampingRecipe;<init>(Lnet/minecraft/item/crafting/Ingredient;Lnet/minecraftforge/fluids/FluidStack;Lnet/minecraft/item/crafting/Ingredient;Lnet/minecraft/item/ItemStack;)V"))
+//    private void stampGearRecipe(ItemStampingRecipe instance, Ingredient ingredient, FluidStack fluidStack, Ingredient stamp, ItemStack output) {
+//        new ItemStampingRecipe(ingredient, FluidRegistry.getFluidStack(this.name.toLowerCase(),ConfigMachine.STAMPER.stampGearAmount * ConfigMisc.ingotFluidAmount), Ingredient.fromItem(ItemRegister.STAMP_GEAR), output);
+//    }
+}
